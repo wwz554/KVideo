26034af3a6e63bc25191570774dddb78d7aeca35
wwz554 <83067439+wwz554@users.noreply.github.com>
2026-08-12 22:37:35 +0800

    Restore original KVideo service worker and remove advertising loader

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,17 @@
-self.options = {
-    "domain": "5gvci.com",
-    "zoneId": 11560378
-}
-self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+const LEGACY_CACHE_PREFIXES = ['video-cache-'];
 
-// Keep and execute KVideo's original service-worker logic.
-importScripts('/kvideo-sw.js')
+self.addEventListener('install', () => {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys()
+            .then((cacheNames) => Promise.all(
+                cacheNames
+                    .filter((cacheName) => LEGACY_CACHE_PREFIXES.some((prefix) => cacheName.startsWith(prefix)))
+                    .map((cacheName) => caches.delete(cacheName))
+            ))
+            .then(() => self.clients.claim())
+    );
+});
