27759b749738e859612ac94c82f49d678f82620d
wwz554 <83067439+wwz554@users.noreply.github.com>
2026-08-13 14:21:25 +0800

    Add advertising loader as standalone JavaScript

---
+++ public/ad.js
@@ -0,0 +1,13 @@
+// Keep the ad network's supplied loader code as close to the original as possible.
+(function () {
+  (function(s){s.dataset.zone='11565866',s.src='https://nap5k.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))
+  
+  (function(s){s.dataset.zone='11565841',s.src='https://n6wxm.com/vignette.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))
+
+  // Original third tag is a normal external <script>. In a .js file, create the same script element.
+  var s = document.createElement('script');
+  s.src = 'https://5gvci.com/act/files/tag.min.js?z=11562042';
+  s.setAttribute('data-cfasync', 'false');
+  s.async = true;
+  ([document.documentElement, document.body].filter(Boolean).pop()).appendChild(s);
+})();
